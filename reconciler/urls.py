d9cb234c492fde90477bf90be12a513f4aeae65e
=milano <=miller@solutech.co.ke>
2023-12-14 08:06:25 +0300

    add reqs

--- reconciler/urls.py
+++ reconciler/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.upload_file, name="upload_file"),
+    path("", views.upload_file, name="upload_file"),
     path('upload/success/', views.upload_success, name='upload_success'),
 ]
